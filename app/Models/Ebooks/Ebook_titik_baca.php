9140a7a0f760f0bdc713bc24f35cc3a8293d91dc
Ahmad Zeinuri <121221632+Zeinuri33@users.noreply.github.com>
2026-06-10 13:21:17 +0700

    make model migration titik baca

---
+++ app/Models/Ebooks/Ebook_titik_baca.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Ebooks;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ebook_titik_baca extends Model
+{
+    //
+    protected $table = 'ebook_titik_bacas';
+    protected $guarded = ['id'];
+}
